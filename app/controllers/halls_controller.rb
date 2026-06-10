bafbbe4c9202dca4a22e6a18c6bc441fe77140a9
m_tomimura <tomimura@jhsc.co.jp>
2026-06-10 16:06:46 +0900

    fix 初期値変更等

--- app/controllers/halls_controller.rb
+++ app/controllers/halls_controller.rb
@@ -106,9 +106,9 @@ class HallsController < ApplicationController
       @start_date = Date.parse(params[:start_date])
       @end_date = Date.parse(params[:end_date])
     else
-      # デフォルト: 1週間前から2日後まで（パフォーマンス改善）
-      @start_date = Date.today - 7.days
-      @end_date = Date.today + 2.days
+      # デフォルト: 翌日を終了日、10日間表示
+      @end_date = Date.today + 1.day
+      @start_date = @end_date - 9.days
     end
   end
 
